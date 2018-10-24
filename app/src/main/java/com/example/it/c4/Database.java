3c612ff04c0902f07009d828e2d9220b2e946c04
Uyen <xuyennguyenx@gmail.com>
2018-10-24 13:52:26 -0400

    Stuff with some IP address set up. Using this tutorial for help: https://www.youtube.com/watch?v=9Rwopuah2Q0

---
+++ app/src/main/java/com/example/it/c4/Database.java
@@ -0,0 +1,20 @@
+//package com.example.it.c4;
+//
+//import android.os.AsyncTask;
+//import android.os.Bundle;
+//import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.widget.RecyclerView;
+//
+//
+//public class Database extends AppCompatActivity {
+//	RecyclerView databaseView;
+//
+//	protected void onCreate(Bundle savedInstanceState){
+//		super.onCreate(savedInstanceState);
+//		setContentView(R.layout.activity_main);
+//
+//		databaseView = (RecyclerView)findViewById(R.id.DatabaseList);
+//		downloadJSON("http:/10.10.10.168/Admin_group.php");
+//
+//	}
+//}
--- app/src/main/java/com/example/it/c4/MainActivity.java
+++ app/src/main/java/com/example/it/c4/MainActivity.java
@@ -1,24 +1,42 @@
 package com.example.it.c4;
 
 import android.media.MediaPlayer;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
+
+	private static Socket socket;
+//	private static ServerSocket serverSocket;
+//	private static InputStreamReader inputStreamReader;
+//	private static BufferedReader bufferedReader;
+	private static PrintWriter printWriter;
+
+
+
+	String message = "";
+	private static String ip = "10.10.10.222";
+	EditText connectionTxt;
+
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-
+		// Ringing Button
 		Button AC_button = findViewById(R.id.AC_button);
 		final MediaPlayer ringing = MediaPlayer.create(this, R.raw.ringing);
-
 
 		AC_button.setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -26,6 +44,13 @@ public class MainActivity extends AppCompatActivity {
 				ringing(ringing);
 			}
 		});
+
+
+		// Connection Test stuff
+		connectionTxt = findViewById(R.id.connectionTestTxt);
+
+
+
 	}
 
 	protected void ringing(MediaPlayer ringing){
@@ -45,6 +70,34 @@ public class MainActivity extends AppCompatActivity {
 		}
 	}
 
+	protected void connetionTest(View v){
+		message = connectionTxt.getText().toString();
+		myTask myTasks = new myTask();
+		myTasks.execute();
+
+		Toast.makeText(getApplicationContext(), "Data sent", Toast.LENGTH_LONG).show();
+
+
+
+	}
+
+	class myTask extends AsyncTask<Void, Void, Void> {
+		@Override
+		protected Void doInBackground(Void... voids) {
+
+			try {
+				socket = new Socket(ip, 5000);     // Connect to the socket at port 5000
+				printWriter = new PrintWriter(socket.getOutputStream());        // set the output stream
+				printWriter.write(message);             // Send message through?
+				printWriter.flush();
+				printWriter.close();
+				socket.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return null;
+		}
+	}
 }
 
 
